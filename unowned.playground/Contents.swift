8effe05e6fe96dba92e18ccb3708c3c171db4f8a
Vitaly <qulix.vitalik.kazakevich@gmail.com>
2018-03-26 15:40:07 +0300

    unowned added

---
+++ unowned.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class Customer {
+    let name: String
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+    let number: Int
+    unowned let customer: Customer
+    init(number: Int, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { print("Card #\(number) is being deinitialized") }
+}
+
+var john: Customer?
+
+john = Customer(name: "John Appleseed")
+john!.card = CreditCard(number: 1234_5678_9012_3456, customer: john!)
+john = nil
+// in fact unowned is the same as weak!
+
