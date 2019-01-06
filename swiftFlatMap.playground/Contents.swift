f0d4c63fc47f40c06a459629613009d4373737a7
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2019-01-06 11:49:32 +0300

    Compact map in sequence

--- swiftFlatMap.playground/Contents.swift
+++ swiftFlatMap.playground/Contents.swift
@@ -6,11 +6,11 @@ var str = "Hello, playground"
 let myAwesomeArr: [[String]?]? = [["A", "B"], nil, ["D"], ["E"]]
 
 
-let transformedArr1 = myAwesomeArr.flatMap { $0 }
+let transformedArr1 = myAwesomeArr.flatMap { $0 } // from optional
 print(transformedArr1 ?? "")
 
-let transformedArr2 = myAwesomeArr?.flatMap { $0 }
+let transformedArr2 = myAwesomeArr?.compactMap { $0 } // was flatMap
 print(transformedArr2 ?? "")
 
-let transformedArr3 = myAwesomeArr?.flatMap { $0 ?? [] }
+let transformedArr3 = myAwesomeArr?.flatMap { $0 ?? [] } // equivalent to Array(s.map(transform).joined()).
 print(transformedArr3 ?? "")
